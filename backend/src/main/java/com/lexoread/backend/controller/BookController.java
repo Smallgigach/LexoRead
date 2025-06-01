a79d702e2129fb2b9585c1cdedb41e2e9b6bd9b1
levchik2011 <kartsev.python@mail.ru>
2025-06-01 21:05:38 +0500

    CRUD для книг

--- backend/src/main/java/com/lexoread/backend/controller/BookController.java
+++ backend/src/main/java/com/lexoread/backend/controller/BookController.java
@@ -1,5 +1,9 @@
 package com.lexoread.backend.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class BookController {
     
 }
--- backend/src/main/java/com/lexoread/backend/model/Book.java
+++ backend/src/main/java/com/lexoread/backend/model/Book.java
@@ -1,5 +1,20 @@
 package com.lexoread.backend.model;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String URLpdf;
 }
+
