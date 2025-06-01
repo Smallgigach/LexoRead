fc2a5e1e08d5d6799ac22dbdbbf8dacff3a8062b
Ligfi <149589876+Lev2011-karcev@users.noreply.github.com>
2025-06-01 21:11:31 +0500

    Update BookRepository.java

--- backend/src/main/java/com/lexoread/backend/repository/BookRepository.java
+++ backend/src/main/java/com/lexoread/backend/repository/BookRepository.java
@@ -1,5 +1,12 @@
 package com.lexoread.backend.repository;
 
-public class BookRepository {
-    
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lexoread.backend.model.Book;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+	
 }
