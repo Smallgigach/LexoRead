c45d4f3fad862795886bd6a97b9b516be9d37367
Ligfi <149589876+Lev2011-karcev@users.noreply.github.com>
2025-06-02 21:00:14 +0500

    Update BookRepository.java

--- backend/src/main/java/com/lexoread/backend/repository/BookRepository.java
+++ backend/src/main/java/com/lexoread/backend/repository/BookRepository.java
@@ -1,12 +1,18 @@
 package com.lexoread.backend.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.lexoread.backend.model.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-	
+	@Query(value = "SELECT * FROM book LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<Book> findBooksWithLimitOffset(@Param("limit") int limit, @Param("offset") int offset);
+
 }
